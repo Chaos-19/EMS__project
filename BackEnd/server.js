182725d437af48bac5a7f69ea3aea3b80d08985b
Natty-45 <natty.ayalew.code@gmail.com>
2025-02-11 22:42:41 +0300

    Add static file serving for uploads; configure express to handle file uploads and set up directory for serving static files

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,15 +5,22 @@ import authRoute from "./routes/auth.route.js";
 import eventRoute from "./routes/event.route.js";
 import userRoute from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", authRoute);
 app.use("/api/event", eventRoute);
 app.use("/api/user", userRoute);
